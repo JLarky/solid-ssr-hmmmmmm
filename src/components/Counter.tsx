8aa46c39e5dc196b7a734b01ac4878cbc6d3d9cc
Agata Yurina <aga@fogbender.com>
2023-03-17 13:27:02 -0600

    SSR WTH

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,31 @@
-import { createSignal } from "solid-js";
+import { createEffect, createMemo, createSignal } from "solid-js";
 import "./Counter.css";
 
+let i = Math.random();
+const ssr = import.meta.env.SSR;
+
+console.log("ssr", ssr);
+
 export default function Counter() {
-  const [count, setCount] = createSignal(0);
+  const [count, setCount] = createSignal(i);
+  const [flag, toggleFlag] = createSignal(ssr);
+  createEffect(() => {
+    console.log("why is this value not match the text?", count());
+  });
+  createEffect(() => {
+    console.log(
+      "first click doesnt' cause UI change, but causes effect",
+      flag()
+    );
+  });
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
-      Clicks: {count()}
-    </button>
+    <>
+      <button class="increment" onClick={() => setCount(count() + 1)}>
+        Clicks: {count()}
+      </button>
+      <button class="increment" onClick={() => toggleFlag(!flag())}>
+        Toggle: {flag() ? "true" : "false"}
+      </button>
+    </>
   );
 }
